a66db3ea6b8f0ab08c3737ea82d53785a9c06f64
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-02-17 21:35:38 -0500

    done

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, setToys}) {
+  function handleDeleteToy(deletedToy){
+    const updatedToys = toys.filter((toy)=> toy.id !== deletedToy.id)
+    setToys(updatedToys)
+  }
+
+  function handleAddLike(updatedToy){
+    const updatedToys = toys.map((toy)=>{
+      if (toy.id === updatedToy.id){
+        return updatedToy
+      } else {
+        return toy
+      }
+    })
+    setToys(updatedToys)
+  }
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy)=>(
+        <ToyCard
+          key={toy.id} toy={toy}
+          onDeleteToy={handleDeleteToy}
+          onAddLike={handleAddLike}
+        />
+      ))}
+    </div>
   );
 }
 
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({onAddToy}) {
+  const [name, setName] = useState('')
+  const [image, setImage]= useState('https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg')
+  const [likes, setLikes] = useState(0)
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const toyData = {
+      name : name,
+      image : image,
+      likes: likes
+    }
+    fetch(`http://localhost:3001/toys`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toyData),
+    })
+    .then((r)=>r.json())
+    .then((newToy)=> onAddToy(newToy))
+  }
+
   return (
     <div className="container">
       <form className="add-toy-form">
@@ -10,6 +32,7 @@ function ToyForm() {
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={(e) => setName(e.target.value)}
         />
         <br />
         <input
@@ -17,6 +40,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={(e)=> setImage(e.target.value)}
         />
         <br />
         <input
@@ -24,6 +48,7 @@ function ToyForm() {
           name="submit"
           value="Create New Toy"
           className="submit"
+          onClick={handleSubmit}
         />
       </form>
     </div>
